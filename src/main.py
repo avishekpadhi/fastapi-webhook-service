882fb9e8c53012f54f0d7898d75dc2d1fcfd973c
= <abhishek.padhi@akrity.com>
2025-10-28 23:46:56 +0530

    feat: implemeted the required API endpoint

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
+from .models import Transaction
 
 from .db import get_db
 
@@ -19,3 +20,27 @@ def test_db(db: Session = Depends(get_db)):
         return {"status": "connected ✅"}
     except Exception as e:
         return {"status": "error ❌", "details": str(e)}
+
+@app.post("/v1/webhooks/transactions")
+async def receive_webhook(request: Request, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    payload = await request.json()
+    
+    response = {"message": "Webhook received"}
+    
+    background_tasks.add_task(process_transaction, payload, db)
+    
+    return response
+
+def process_transaction(payload: dict, db: Session):
+    try:
+        transaction = Transaction(
+            transaction_id=payload.get("transaction_id"),
+            source_account=payload.get("source_account"),
+            amount=payload.get("amount"),
+            status=payload.get("status"),
+        )
+        db.add(transaction)
+        db.commit()
+        print("✅ Transaction stored successfully:", payload)
+    except Exception as e:
+        print("❌ Error inserting transaction:", str(e))
---
+++ src/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Numeric, TIMESTAMP, text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import declarative_base
+import uuid
+
+Base = declarative_base()
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
+    transaction_id = Column(String, nullable=False)
+    source_account = Column(String, nullable=False)
+    amount = Column(Numeric, nullable=False)
+    status = Column(String, nullable=False)
